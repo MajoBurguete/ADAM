7546c279ec9dc134cff81894aead298385977e85
AaronCG01 <A01730451@itesm.mx>
2021-09-07 18:29:23 -0500

    First commit shapesActivityLayoutNVL3 07-09-2021 18:29 Issue #13

--- app/src/main/java/mx/tec/a01730344/adam/shapesActivityLayoutNVL3.java
+++ app/src/main/java/mx/tec/a01730344/adam/shapesActivityLayoutNVL3.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class shapesActivityLayout extends AppCompatActivity {
+public class shapesActivityLayoutNVL3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shapes_layout);
+        setContentView(R.layout.activity_shapes_layout_nvl_3);
     }
 }
