e9a5d0a2d671218de2798c48f5641231fe406c7d
MajoBurguete <A01730344@itesm.mx>
2021-09-09 16:46:36 -0600

    Hot fix

---
+++ app/src/main/java/mx/tec/a01730344/adam/RainbowActivityNVL2.java
@@ -0,0 +1,13 @@
+package mx.tec.a01730344.adam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RainbowActivityNVL2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
