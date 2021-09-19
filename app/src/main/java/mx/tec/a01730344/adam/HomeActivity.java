18d36e9d8a1aa97a30d6314de91ff29269e283a5
MajoBurguete <A01730344@itesm.mx>
2021-09-19 01:26:51 -0600

    Intent to profile #44

--- app/src/main/java/mx/tec/a01730344/adam/HomeActivity.java
+++ app/src/main/java/mx/tec/a01730344/adam/HomeActivity.java
@@ -1,14 +1,29 @@
 package mx.tec.a01730344.adam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ConstraintLayout clToProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        clToProfile = findViewById(R.id.clToProfile);
+
+        clToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toProfile = new Intent(HomeActivity.this, UserProfileActivity.class);
+                startActivity(toProfile);
+            }
+        });
     }
 }
