d3da444026f439880bdb16ebf6e4ff70879765da
AaronCG01 <A01730451@itesm.mx>
2021-10-10 23:04:28 -0500

    Story game logic done 10-10-2021 23:02 Issue #71

--- app/src/main/java/mx/tec/a01730344/adam/StoryActivity.java
+++ app/src/main/java/mx/tec/a01730344/adam/StoryActivity.java
@@ -1,14 +1,313 @@
 package mx.tec.a01730344.adam;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StoryActivity extends AppCompatActivity {
+
+    StoryModelNVL1 model1;
+    StoryModelNVL2AndNVL3 model2And3;
+    ConstraintLayout clStory;
+    ConstraintLayout clQuestion;
+    TextView tvStory;
+    TextView tvQuestion;
+    TextView tvTrueFalse;
+    TextView tvScoreS;
+    Button btnContinue;
+    Button btnNextQ;
+    Button btnFirstOption;
+    Button btnSecondOption;
+    Button btnThirdOption;
+    ImageView ivLife1S;
+    ImageView ivLife2S;
+    ImageView ivLife3S;
+    int lives = 3;
+    int difficulty = 1;
+    String strScore;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        model1 = new StoryModelNVL1(this);
+        model2And3 = new StoryModelNVL2AndNVL3(this);
+        clStory = findViewById(R.id.clStory);
+        clQuestion = findViewById(R.id.clQuestion);
+        tvStory = findViewById(R.id.tvStory2);
+        btnContinue = findViewById(R.id.btnContinue);
+        btnNextQ = findViewById(R.id.btnNextQ);
+        btnFirstOption = findViewById(R.id.btnOptionS1);
+        btnSecondOption = findViewById(R.id.btnOptionS2);
+        btnThirdOption = findViewById(R.id.btnOptionS3);
+        tvQuestion = findViewById(R.id.tvQuestionS);
+        tvTrueFalse = findViewById(R.id.tvResult);
+        tvScoreS = findViewById(R.id.tvScoreS);
+        ivLife1S = findViewById(R.id.ivLife1S);
+        ivLife2S = findViewById(R.id.ivLife2S);
+        ivLife3S = findViewById(R.id.ivLife3S);
+        //difficulty = getIntent().getExtras().getInt("difficulty");
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (difficulty == 1) {
+                    clStory.setVisibility(View.GONE);
+                    clQuestion.setVisibility(View.VISIBLE);
+                    model1.generateQuestionAndAnswers();
+                    tvQuestion.setText(model1.question);
+                    btnFirstOption.setText(model1.firstAnswerOption);
+                    btnSecondOption.setText(model1.secondAnswerOption);
+                    btnThirdOption.setText(model1.thirdAnswerOption);
+                    btnNextQ.setEnabled(false);
+                    btnFirstOption.setEnabled(true);
+                    btnSecondOption.setEnabled(true);
+                    btnThirdOption.setEnabled(true);
+                    btnFirstOption.setBackgroundColor(getResources().getColor(R.color.verde_pasto));
+                    btnSecondOption.setBackgroundColor(getResources().getColor(R.color.naranja_claro));
+                    btnThirdOption.setBackgroundColor(getResources().getColor(R.color.azul_oceano));
+                }
+                else {
+                    if (model2And3.actualParagraph <= 2) {
+                        tvStory.setText(model2And3.generateNewParagraph(difficulty));
+                    }
+                    else {
+                        clStory.setVisibility(View.GONE);
+                        clQuestion.setVisibility(View.VISIBLE);
+                        model2And3.generateQuestionAndAnswers();
+                        tvQuestion.setText(model2And3.question);
+                        btnFirstOption.setText(model2And3.firstAnswerOption);
+                        btnSecondOption.setText(model2And3.secondAnswerOption);
+                        btnThirdOption.setText(model2And3.thirdAnswerOption);
+                        btnNextQ.setEnabled(false);
+                        btnFirstOption.setEnabled(true);
+                        btnSecondOption.setEnabled(true);
+                        btnThirdOption.setEnabled(true);
+                        btnFirstOption.setBackgroundColor(getResources().getColor(R.color.verde_pasto));
+                        btnSecondOption.setBackgroundColor(getResources().getColor(R.color.naranja_claro));
+                        btnThirdOption.setBackgroundColor(getResources().getColor(R.color.azul_oceano));
+                    }
+                }
+            }
+        });
+
+        btnNextQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (difficulty == 1) {
+                    if (lives > 0 && model1.actualQuestion <= 2) {
+                        model1.generateQuestionAndAnswers();
+                        tvTrueFalse.setText("");
+                        tvQuestion.setText(model1.question);
+                        btnFirstOption.setText(model1.firstAnswerOption);
+                        btnSecondOption.setText(model1.secondAnswerOption);
+                        btnThirdOption.setText(model1.thirdAnswerOption);
+                        btnNextQ.setEnabled(false);
+                        btnFirstOption.setEnabled(true);
+                        btnSecondOption.setEnabled(true);
+                        btnThirdOption.setEnabled(true);
+                        btnFirstOption.setBackgroundColor(getResources().getColor(R.color.verde_pasto));
+                        btnSecondOption.setBackgroundColor(getResources().getColor(R.color.naranja_claro));
+                        btnThirdOption.setBackgroundColor(getResources().getColor(R.color.azul_oceano));
+                    }
+                    else {
+                        Intent toHome = new Intent(StoryActivity.this, HomeActivity.class);
+                        startActivity(toHome);
+                    }
+                }
+                else {
+                    if (lives > 0 && model2And3.actualQuestion <= 2) {
+                        model2And3.generateQuestionAndAnswers();
+                        tvTrueFalse.setText("");
+                        tvQuestion.setText(model2And3.question);
+                        btnFirstOption.setText(model2And3.firstAnswerOption);
+                        btnSecondOption.setText(model2And3.secondAnswerOption);
+                        btnThirdOption.setText(model2And3.thirdAnswerOption);
+                        btnNextQ.setEnabled(false);
+                        btnFirstOption.setEnabled(true);
+                        btnSecondOption.setEnabled(true);
+                        btnThirdOption.setEnabled(true);
+                        btnFirstOption.setBackgroundColor(getResources().getColor(R.color.verde_pasto));
+                        btnSecondOption.setBackgroundColor(getResources().getColor(R.color.naranja_claro));
+                        btnThirdOption.setBackgroundColor(getResources().getColor(R.color.azul_oceano));
+                    }
+                    else {
+                        Intent toHome = new Intent(StoryActivity.this, HomeActivity.class);
+                        startActivity(toHome);
+                    }
+                }
+            }
+        });
+
+        btnFirstOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(0);
+            }
+        });
+
+        btnSecondOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(1);
+            }
+        });
+
+        btnThirdOption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(2);
+            }
+        });
+
+        if (difficulty == 1) {
+            tvStory.setText(model1.generateParagraph());
+        }
+        else {
+            tvStory.setText(model2And3.generateNewParagraph(difficulty));
+        }
+    }
+
+    private void checkAnswer(int option) {
+        Button btnOption1 = btnFirstOption, btnOption2 = btnSecondOption, btnOption3 = btnThirdOption;
+        if (option == 1) {
+            btnOption1 = btnSecondOption;
+            btnOption2 = btnFirstOption;
+            btnOption3 = btnThirdOption;
+        }
+        if (option == 2) {
+            btnOption1 = btnThirdOption;
+            btnOption2 = btnFirstOption;
+            btnOption3 = btnSecondOption;
+        }
+        String trueOrFalse = "";
+        if (difficulty == 1) {
+            if (model1.actualQuestion == 0) {
+                if (btnOption1.getText() == model1.firstAns) {
+                    trueOrFalse = "¡Muy bien!";
+                } else {
+                    trueOrFalse = "¡Intenta de nuevo!";
+                }
+            }
+            if (model1.actualQuestion == 1) {
+                if (btnOption1.getText() == model1.secondAns) {
+                    trueOrFalse = "¡Muy bien!";
+                } else {
+                    trueOrFalse = "¡Intenta de nuevo!";
+                }
+            }
+            if (model1.actualQuestion == 2) {
+                if (btnOption1.getText() == model1.thirdAns) {
+                    trueOrFalse = "¡Muy bien!";
+                } else {
+                    trueOrFalse = "¡Intenta de nuevo!";
+                }
+            }
+        }
+        else {
+            if (model2And3.actualQuestion == 0) {
+                if (btnOption1.getText() == model2And3.firstParagraphAns) {
+                    trueOrFalse = "¡Muy bien!";
+                } else {
+                    trueOrFalse = "¡Intenta de nuevo!";
+                }
+            }
+            if (model2And3.actualQuestion == 1) {
+                if (btnOption1.getText() == model2And3.secondParagraphAns) {
+                    trueOrFalse = "¡Muy bien!";
+                } else {
+                    trueOrFalse = "¡Intenta de nuevo!";
+                }
+            }
+            if (model2And3.actualQuestion == 2) {
+                if (btnOption1.getText() == model2And3.thirdParagraphAns) {
+                    trueOrFalse = "¡Muy bien!";
+                } else {
+                    trueOrFalse = "¡Intenta de nuevo!";
+                }
+            }
+        }
+        tvTrueFalse.setText(trueOrFalse);
+        if (trueOrFalse.equals("¡Muy bien!")) {
+            score += lives * difficulty * 100;
+            strScore = "Puntuación: " + score;
+            tvScoreS.setText(strScore);
+            btnOption1.setBackgroundColor(getResources().getColor(R.color.verde));
+            btnOption2.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+            btnOption3.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+            btnOption1.setEnabled(false);
+            btnOption2.setEnabled(false);
+            btnOption3.setEnabled(false);
+            btnNextQ.setEnabled(true);
+            model1.actualQuestion ++;
+            model2And3.actualQuestion ++;
+        }
+        else {
+            lives --;
+            if (lives == 2) {
+                ivLife3S.setImageResource(R.drawable.ic_adam_dead);
+            }
+            if (lives == 1) {
+                ivLife2S.setImageResource(R.drawable.ic_adam_dead);
+            }
+            if (lives == 0) {
+                ivLife1S.setImageResource(R.drawable.ic_adam_dead);
+                String gameOver = "¡Juego Terminado!";
+                tvTrueFalse.setText(gameOver);
+                btnOption1.setEnabled(false);
+                btnOption2.setEnabled(false);
+                btnOption3.setEnabled(false);
+                btnOption1.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                if (difficulty == 1) {
+                    if (model1.actualQuestion == 1) {
+                        if (btnOption2.getText().equals(model1.secondAns)) {
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        } else {
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        }
+                    } else {
+                        if (btnOption2.getText().equals(model1.thirdAns)) {
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        } else {
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        }
+                    }
+                }
+                else {
+                    if (model2And3.actualQuestion == 1) {
+                        if (btnOption2.getText().equals(model2And3.secondParagraphAns)) {
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        } else {
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        }
+                    } else {
+                        if (btnOption2.getText().equals(model2And3.thirdParagraphAns)) {
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        } else {
+                            btnOption3.setBackgroundColor(getResources().getColor(R.color.verde));
+                            btnOption2.setBackgroundColor(getResources().getColor(R.color.r_rojo));
+                        }
+                    }
+                }
+                btnNextQ.setEnabled(true);
+            }
+            else {
+                btnOption1.setEnabled(false);
+            }
+        }
     }
 }
---
+++ app/src/main/java/mx/tec/a01730344/adam/StoryModelNVL1.java
@@ -0,0 +1,130 @@
+package mx.tec.a01730344.adam;
+
+import android.content.Context;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
+import androidx.core.content.ContextCompat;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class StoryModelNVL1 {
+
+    Context context;
+    int randomStory;
+    boolean paragraphGenerated = false;
+    int actualQuestion = 0;
+    String firstAns;
+    String secondAns;
+    String thirdAns;
+    String question;
+    String firstAnswerOption;
+    String secondAnswerOption;
+    String thirdAnswerOption;
+    List<String> firstOptionsPool;
+    List<String> secondOptionsPool;
+    List<String> thirdOptionsPool;
+    List<List<String>> stories =
+            Arrays.asList(Arrays.asList("Caperucita era una niña de # años que vivía cerca del bosque, en una casa #. Un día, # le pidió que le llevara comida a su abuelita.", "Caperucita llenó una cesta con # y salió # de su casa. Por el camino, se encontró con #.", "De repente, un lobo # apareció. Asustó a caperucita con sus #, haciéndola correr de regreso a su casa por el #."),
+                    Arrays.asList("Había una vez un conejo # y una tortuga que participaron en una carrera de # vueltas por la #.", "El conejo se burlaba de la tortuga con su amiga #, diciendo: \"eres más lenta que un #, en cambio yo soy tan rápido como el #\".", "Al iniciar, el conejo rebasó a la tortuga y se sentó a #, confiado. La tortuga lo pasó, ganando #. Al llegar el conejo se enojó y #, derrotado."),
+                    Arrays.asList("Había una vez una princesa con un vestido #, que tenía un perrito llamado #. Pero un día lo fue a ver # y había desaparecido.", "Fue a preguntarle # si lo había visto, pero no sabía dónde estaba. Corrió por el #, cuando encontró huellas que la guiaban al #.", "Encontró una cueva, donde estaba su perro y # más. Parecían atrapados por unas #. Al final, la princesa construyó # y los rescató."),
+                    Arrays.asList("Había una vez un pastorcito llamado # que cuidaba a su rebaño en la #. Un día # estaba aburrido, cuando se le ocurrió gritar \"¡lobo!\" como broma.", "Al escucharlo, # corrieron de prisa para ayudarlo. Pero al llegar, no encontraron a ningún lobo. #, regresaron a sus #. Esto pasó varias veces.", "A la #, las personas decidieron no volver a creerle. Pero # llegó un lobo de verdad. El niño gritó de nuevo, pero nadie vino. El lobo se comió # ovejas."),
+                    Arrays.asList("Había una vez un zorro que buscaba algo de comer, cuando vio a un cuervo en un # muy alto con un pedazo de # en su # pico que se le antojó.", "Decidió usar su astucia para conseguirlo, saludando al cuervo, el cual lo miró #. \"¡Gran rey cuervo!, qué # plumas y qué # figura tienes!\"", "Los halagos hicieron al cuervo abrir el pico para #, pero su comida cayó justo en la boca del zorro, quien salió corriendo hacia #. Él iba # porque había logrado su meta."));
+    List<List<List<List<String>>>> variants =
+            Arrays.asList(Arrays.asList(Arrays.asList(Arrays.asList("5", "8", "10"), Arrays.asList("azul", "morada", "rosa"), Arrays.asList("su mamá", "su papá", "su hermana")), Arrays.asList(Arrays.asList("fresas", "manzanas", "peras"), Arrays.asList("corriendo", "caminando", "saltando"), Arrays.asList("una rosa", "una dalia", "una margarita")), Arrays.asList(Arrays.asList("feroz", "astuto", "mentiroso"), Arrays.asList("garras", "colmillos", "aullidos"), Arrays.asList("bosque", "prado", "valle"))),
+                    Arrays.asList(Arrays.asList(Arrays.asList("blanco", "gris", "café"), Arrays.asList("2", "3", "5"), Arrays.asList("montaña", "isla", "selva")), Arrays.asList(Arrays.asList("la rana", "la cebra", "la serpiente"), Arrays.asList("caracol", "perezoso", "koala"), Arrays.asList("viento", "rayo", "río")), Arrays.asList(Arrays.asList("comer", "dormir", "jugar"), Arrays.asList("un listón", "un trofeo", "una medalla"), Arrays.asList("gritó", "lloró", "suspiró"))),
+                    Arrays.asList(Arrays.asList(Arrays.asList("dorado", "rojo", "morado"), Arrays.asList("Max", "Rocky", "Toby"), Arrays.asList("al jardín", "al patio", "al gran salón")), Arrays.asList(Arrays.asList("a su padre", "a su madrastra", "a su guardia"), Arrays.asList("pasillo", "puente", "jardín"), Arrays.asList("pueblo", "sótano", "pozo")), Arrays.asList(Arrays.asList("2", "5", "8"), Arrays.asList("rocas", "cajas", "tablas"), Arrays.asList("una escalera", "una rampa", "una polea"))),
+                    Arrays.asList(Arrays.asList(Arrays.asList("Pablo", "Juan", "Pedro"), Arrays.asList("montaña", "pradera", "colina"), Arrays.asList("en la mañana", "en la tarde", "en la noche")), Arrays.asList(Arrays.asList("sus amigos", "sus familiares", "sus vecinos"), Arrays.asList("Frustrados", "Enojados", "Confundidos"), Arrays.asList("casas", "granjas", "actividades")), Arrays.asList(Arrays.asList("tercera vez", "cuarta vez", "quinta vez"), Arrays.asList("una tarde", "una noche", "una mañana"), Arrays.asList("3", "6", "9"))),
+                    Arrays.asList(Arrays.asList(Arrays.asList("árbol", "arbusto", "poste"), Arrays.asList("queso", "manzana", "sandía"), Arrays.asList("grande", "pequeño", "ancho")), Arrays.asList(Arrays.asList("desconfiado", "inseguro", "irritado"), Arrays.asList("magníficas", "brillantes", "coloridas"), Arrays.asList("hermosa", "espléndida", "increíble")), Arrays.asList(Arrays.asList("reír", "sonreír", "cantar"), Arrays.asList("su madriguera", "su cueva", "su escondite"), Arrays.asList("contento", "saciado", "complacido"))));
+    List<List<List<String>>> questions =
+            Arrays.asList(Arrays.asList(Arrays.asList("¿Cuántos años tenía Caperucita?", "¿De qué color era la casa de Caperucita?", "¿Quién le pidió a Caperucita que llevara comida a su abuelita?"), Arrays.asList("¿Qué llevaba la cesta?", "¿Cómo salió Caperucita de su casa?", "¿Qué flor se encontró Caperucita por el camino?"), Arrays.asList("¿Cómo era el lobo que apareció?", "El lobo asustó a Caperucita con sus...", "¿Por dónde regresó Caperucita a su casa?")),
+                    Arrays.asList(Arrays.asList("¿De qué color era el conejo?", "¿De cuántas vueltas era la carrera?", "La carrera se hizo en la..."), Arrays.asList("¿Quién era amiga del conejo?", "El conejo decía que la tortuga era más lenta que un…", "El conejo decía que era tan rápido como el..."), Arrays.asList("El conejo confiado se sentó a…", "¿Qué ganó la tortuga?", "Cuando perdió, el conejo se enojó y...")),
+                    Arrays.asList(Arrays.asList("¿De qué color era el vestido de la princesa?", "¿Cómo se llamaba el perrito de la princesa?", "¿A dónde fue la princesa para buscar a su perrito?"), Arrays.asList("¿A quién le preguntó la princesa si había visto a su perrito?", "La princesa corrió por el…", "La princesa encontró huellas que la guiaban al..."), Arrays.asList("Además del perrito de la princesa, ¿cuántos más había en la cueva?", "Los perritos estaban atrapados por unas…", "¿Qué construyó la princesa para rescatar a los perritos?")),
+                    Arrays.asList(Arrays.asList("¿Cómo se llamaba el pastorcito?", "El pastorcito cuidaba a sus ovejas en la…", "¿Cuándo hizo la broma el pastorcito?"), Arrays.asList("¿Quiénes corrieron de prisa para ayudar al pastorcito?", "¿Cómo se sintieron los que habían corrido a ayudar por no encontrar al lobo?", "Las personas regresaron a sus..."), Arrays.asList("Las personas ya no le creyeron al pastorcito a la…", "¿Cuándo llegó un lobo de verdad?", "¿Cuántas ovejas se comió el lobo?")),
+                    Arrays.asList(Arrays.asList("El cuervo estaba en un…", "El cuervo tenía un pedazo de…", "¿Cómo era el pico del cuervo?"), Arrays.asList("¿Cómo miró el cuervo al zorro?", "El zorro le dijo al cuervo que sus plumas eran…", "El zorro le dijo al cuervo que tenía una figura..."), Arrays.asList("Luego de los halagos, el cuervo abrió su pico para…", "¿A dónde se fue corriendo el zorro?", "¿Cómo se sentía el zorro por haber logrado su meta?")));
+
+    public StoryModelNVL1(Context context) {
+        this.context = context;
+    }
+
+    public SpannableStringBuilder generateParagraph() {
+        randomStory = new Random().nextInt(5);
+        StringBuffer parrafo = new StringBuffer(stories.get(randomStory).get(0));
+        int index = 0;
+        char character = parrafo.charAt(index);
+        while (character != '#') {
+            index ++;
+            character = parrafo.charAt(index);
+        }
+        int origin1 = index;
+        parrafo.deleteCharAt(index);
+        int randomVariant1 = new Random().nextInt(3);
+        String firstVariant = variants.get(randomStory).get(0).get(0).get(randomVariant1);
+        parrafo.insert(index, firstVariant);
+        int end1 = index + firstVariant.length();
+        character = parrafo.charAt(index + 1);
+        while (character != '#') {
+            index ++;
+            character = parrafo.charAt(index);
+        }
+        int origin2 = index;
+        parrafo.deleteCharAt(index);
+        int randomVariant2 = new Random().nextInt(3);
+        String secondVariant = variants.get(randomStory).get(0).get(1).get(randomVariant2);
+        parrafo.insert(index, secondVariant);
+        int end2 = index + secondVariant.length();
+        character = parrafo.charAt(index + 1);
+        while (character != '#') {
+            index ++;
+            character = parrafo.charAt(index);
+        }
+        int origin3 = index;
+        parrafo.deleteCharAt(index);
+        int randomVariant3 = new Random().nextInt(3);
+        String thirdVariant = variants.get(randomStory).get(0).get(2).get(randomVariant3);
+        parrafo.insert(index, thirdVariant);
+        int end3 = index + thirdVariant.length();
+        final SpannableStringBuilder ssb = new SpannableStringBuilder(parrafo);
+        final ForegroundColorSpan fcs1 = new ForegroundColorSpan(ContextCompat.getColor(context, R.color.verde_pasto));
+        final ForegroundColorSpan fcs2 = new ForegroundColorSpan(ContextCompat.getColor(context, R.color.naranja_claro));
+        final ForegroundColorSpan fcs3 = new ForegroundColorSpan(ContextCompat.getColor(context, R.color.azul_oceano));
+        ssb.setSpan(fcs1, origin1, end1, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        ssb.setSpan(fcs2, origin2, end2, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        ssb.setSpan(fcs3, origin3, end3, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        firstAns = firstVariant;
+        firstOptionsPool = Arrays.asList(variants.get(randomStory).get(0).get(0).get(0), variants.get(randomStory).get(0).get(0).get(1), variants.get(randomStory).get(0).get(0).get(2));
+        secondAns = secondVariant;
+        secondOptionsPool = Arrays.asList(variants.get(randomStory).get(0).get(1).get(0), variants.get(randomStory).get(0).get(1).get(1), variants.get(randomStory).get(0).get(1).get(2));
+        thirdAns = thirdVariant;
+        thirdOptionsPool = Arrays.asList(variants.get(randomStory).get(0).get(2).get(0), variants.get(randomStory).get(0).get(2).get(1), variants.get(randomStory).get(0).get(2).get(2));
+        paragraphGenerated = true;
+        return ssb;
+    }
+
+    public void generateQuestionAndAnswers() {
+        if (actualQuestion == 0) {
+            question = questions.get(randomStory).get(0).get(0);
+            Collections.shuffle(firstOptionsPool);
+            firstAnswerOption = firstOptionsPool.get(0);
+            secondAnswerOption = firstOptionsPool.get(1);
+            thirdAnswerOption = firstOptionsPool.get(2);
+        }
+        if (actualQuestion == 1) {
+            question = questions.get(randomStory).get(0).get(1);
+            Collections.shuffle(secondOptionsPool);
+            firstAnswerOption = secondOptionsPool.get(0);
+            secondAnswerOption = secondOptionsPool.get(1);
+            thirdAnswerOption = secondOptionsPool.get(2);
+        }
+        if (actualQuestion == 2) {
+            question = questions.get(randomStory).get(0).get(2);
+            Collections.shuffle(thirdOptionsPool);
+            firstAnswerOption = thirdOptionsPool.get(0);
+            secondAnswerOption = thirdOptionsPool.get(1);
+            thirdAnswerOption = thirdOptionsPool.get(2);
+        }
+    }
+
+}
