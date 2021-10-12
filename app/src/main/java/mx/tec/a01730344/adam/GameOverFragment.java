d1ffffa196ea350af95ddedebb1f3f76bdaac0c6
AaronCG01 <A01730451@itesm.mx>
2021-10-12 00:54:23 -0500

    GameOverFragment layout done 12-10-2021 00:54 Issue #77

---
+++ app/src/main/java/mx/tec/a01730344/adam/GameOverFragment.java
@@ -0,0 +1,23 @@
+package mx.tec.a01730344.adam;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameOverFragment extends Fragment {
+
+    public GameOverFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_over, container, false);
+    }
+}
