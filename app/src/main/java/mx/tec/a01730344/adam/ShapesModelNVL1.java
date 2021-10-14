9b96d976675b5e1807f88f3d7e3bed6c753c0e76
AaronCG01 <A01730451@itesm.mx>
2021-10-14 02:20:00 -0500

    ShapesNVL1 model and controller done 14-10-2021 02:17 Issue #81

---
+++ app/src/main/java/mx/tec/a01730344/adam/ShapesModelNVL1.java
@@ -0,0 +1,34 @@
+package mx.tec.a01730344.adam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShapesModelNVL1 {
+
+    int figure1Resource;
+    int figure2Resource;
+    int figure3Resource;
+    int drag1Resource;
+    int drag2Resource;
+    int drag3Resource;
+    boolean state = false;
+    List<Integer> figures = Arrays.asList(R.drawable.ic_circulito_amarillo, R.drawable.ic_circulito_celeste, R.drawable.ic_circulito_naranja, R.drawable.ic_circulito_rosa, R.drawable.ic_circulito_verde, R.drawable.ic_cuadrado_amarillo, R.drawable.ic_cuadrado_celeste, R.drawable.ic_cuadrado_naranja, R.drawable.ic_cuadrado_rosa, R.drawable.ic_cuadrado_verde, R.drawable.ic_estrella_amarilla, R.drawable.ic_estrella_celeste, R.drawable.ic_estrella_naranja, R.drawable.ic_estrella_rosa, R.drawable.ic_estrella_verde, R.drawable.ic_hex_amarillo, R.drawable.ic_hex_celeste, R.drawable.ic_hex_naranja, R.drawable.ic_hex_rosa, R.drawable.ic_hex_verde, R.drawable.ic_triangulo_amarillo, R.drawable.ic_triangulo_celeste, R.drawable.ic_triangulo_naranja, R.drawable.ic_triangulo_rosa, R.drawable.ic_triangulo_verde);
+    List<Integer> drags = new ArrayList<>();
+
+    public void startNewRound() {
+        Collections.shuffle(figures);
+        figure1Resource = figures.get(0);
+        figure2Resource = figures.get(1);
+        figure3Resource = figures.get(2);
+        drags.clear();
+        drags.addAll(figures.subList(0, 3));
+        Collections.shuffle(drags);
+        drag1Resource = drags.get(0);
+        drag2Resource = drags.get(1);
+        drag3Resource = drags.get(2);
+        state = true;
+    }
+
+}
