48be523c709d8e171c3d075141fc6180e38e1360
MajoBurguete <A01730344@itesm.mx>
2021-10-20 13:00:50 -0600

    Final changes, about section was added

---
+++ app/src/main/java/mx/tec/a01730344/adam/Fragments/AboutFragment.java
@@ -0,0 +1,44 @@
+package mx.tec.a01730344.adam.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mx.tec.a01730344.adam.R;
+
+public class AboutFragment extends Fragment {
+    ConstraintLayout clAbout;
+
+    public AboutFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        clAbout = view.findViewById(R.id.clAbout);
+
+        clAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().remove(AboutFragment.this).commit();
+                clAbout.setClickable(false);
+            }
+        });
+    }
+}
